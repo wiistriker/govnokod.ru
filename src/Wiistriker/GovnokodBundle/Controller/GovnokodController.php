941435d28d40d0ea4d82d27d993cff14e47a6144
wiistriker <wiistriker@gmail.com>
2012-04-27 23:47:40 +1100

    add code and code language entity
    simple index list

--- src/Wiistriker/GovnokodBundle/Controller/GovnokodController.php
+++ src/Wiistriker/GovnokodBundle/Controller/GovnokodController.php
@@ -3,12 +3,49 @@
 namespace Wiistriker\GovnokodBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Wiistriker\GovnokodBundle\Entity\Code;
 
 class GovnokodController extends Controller
 {
-    public function addAction()
+    public function listAction()
     {
-        return $this->render('WiistrikerGovnokodBundle:Govnokod:index.html.twig');
+        $repository = $this->getDoctrine()->getRepository('WiistrikerGovnokodBundle:Code');
+        $codes = $repository->findBy(array(), array('created' => 'DESC'));
+
+        return $this->render('WiistrikerGovnokodBundle:Code:list.html.twig', array(
+            'codes' => $codes
+        ));
+    }
+
+    public function addAction(Request $request)
+    {
+        $code = new Code();
+
+        $form = $this->createFormBuilder($code)
+                ->add('text')
+                ->add('description')
+                ->getForm();
+
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                $categoryRepository = $this->getDoctrine()->getRepository('WiistrikerGovnokodBundle:CodeCategory');
+                $category = $categoryRepository->find(1);
+                $code->setCategory($category);
+
+                $em = $this->getDoctrine()->getEntityManager();
+                $em->persist($code);
+                $em->flush();
+
+                return new Response('ok');
+            }
+        }
+
+        return $this->render('WiistrikerGovnokodBundle:Code:add.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 }
--- src/Wiistriker/UserBundle/Entity/User.php
+++ src/Wiistriker/UserBundle/Entity/User.php
@@ -17,4 +17,14 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
