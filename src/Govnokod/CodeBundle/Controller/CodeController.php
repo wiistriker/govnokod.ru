8b859f0b155f429f493bfe3dc60926de135208d7
wiistriker <wiistriker@gmail.com>
2013-01-11 16:20:37 +1100

    view action

--- src/Govnokod/CodeBundle/Controller/CodeController.php
+++ src/Govnokod/CodeBundle/Controller/CodeController.php
@@ -8,11 +8,26 @@ class CodeController extends Controller
 {
     public function listAction()
     {
-        $repository = $this->getDoctrine()->getRepository('GovnokodCodeBundle:Code');
-        $codes = $repository->findBy(array(), array('created_at' => 'DESC'));
+        $codeRepository = $this->getDoctrine()->getRepository('GovnokodCodeBundle:Code');
+        $codes = $codeRepository->findBy(array(), array('created_at' => 'DESC'));
 
         return $this->render('GovnokodCodeBundle:Code:list.html.twig', array(
             'codes' => $codes
+        ));
+    }
+
+    public function viewAction($id)
+    {
+        $codeRepository = $this->getDoctrine()->getRepository('GovnokodCodeBundle:Code');
+
+        $code = $codeRepository->find($id);
+
+        if (!$code) {
+            throw $this->createNotFoundException('Code not found');
+        }
+
+        return $this->render('GovnokodCodeBundle:Code:view.html.twig', array(
+            'code' => $code
         ));
     }
 }
