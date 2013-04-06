04abd808685108a6c23aa90465d76ba21a21c19f
wiistriker <wiistriker@gmail.com>
2013-04-06 13:32:44 +1100

    update codemirror to 3.11

--- www/js/code/save.js
+++ www/js/code/save.js
@@ -2,8 +2,10 @@
     $(function() {
         var codeMirror = CodeMirror.fromTextArea($('#form_body')[0], {
             indentUnit: 4,
+            tabMode: 'spaces',
             lineNumbers: true,
-            autofocus: true
+            autofocus: true,
+            highlightSelectionMatches: true
         }); 
     });
 })(jQuery);
