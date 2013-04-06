18b6521b3ff561cd7d8bb281373ea9844d49d314
wiistriker <wiistriker@gmail.com>
2013-04-06 12:06:20 +1100

    commit before merge

--- src/Govnokod/CodeBundle/Controller/CodeController.php
+++ src/Govnokod/CodeBundle/Controller/CodeController.php
@@ -18,12 +18,17 @@ class CodeController extends Controller
 
     public function viewAction($id)
     {
+        $request = $this->getRequest();
         $codeRepository = $this->getDoctrine()->getRepository('GovnokodCodeBundle:Code');
 
         $code = $codeRepository->find($id);
 
         if (!$code) {
             throw $this->createNotFoundException('Code not found');
+        }
+
+        if ($request->get('_route') == 'view_code_legacy') {
+            return $this->redirect($this->generateUrl('view_code', array('category' => $code->getCategory()->getName(), 'id' => $code->getId())));
         }
 
         return $this->render('GovnokodCodeBundle:Code:view.html.twig', array(
